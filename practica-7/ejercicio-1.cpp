5ec0ca7b84d688540db0d1e32c0c79be5ea8e2da
Marco Olvera <marcolvera333@gmail.com>
2023-09-04 12:11:13 -0600

    practica 7.1 terminada

---
+++ practica-7/ejercicio-1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int desde = 1, hasta = 19;
+
+    if(desde % 2 != 0)
+        desde += 1;
+    if(hasta % 2 != 0)
+        hasta -= 1;
+    for(int i = desde; i<=hasta; i++)
+    {
+            cout<<i<<endl;
+            i++;
+    }
+   
+
+    return 0;
+}
